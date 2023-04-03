2f346425283792087f5f3a9e0ec426aef8fc0b40
Your Name <you@example.com>
2023-04-03 14:55:23 +0200

    Trying to fix invisible table error

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -3,25 +3,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-	let sentMessage = '<script>localStorage.setItem("i", 0)</script>';
+	let sentMessage = '';
 	if (req.query.audioFile) {
 		sentMessage += `<audio id = "audioPlayer" src = ${req.query.audioFile}></audio>`;
 		sentMessage += `<button type="button" id = "audioCancel">cancel audio</button>`;
 		sentMessage += `<button type="button" id = "audioAdd">Add audio</button>`;
 		sentMessage += `<script>document.getElementById("audioCancel").addEventListener('click', 
 		() => document.getElementById("audioPlayer").src = "cancel.mp4")</script>`
-		sentMessage += `<script defer>document.getElementById("audioAdd").addEventListener('click', 
-		() => {
-    		let table = document.getElementById("playlist_table");
-    		let row = table.insertRow(localStorage.i);
-    		let cell1 = row.insertCell(0);
-			let cell2 = row.insertCell(1);
-            let cell3 = row.insertCell(2);
-            cell1.innerHTML = "${localStorage.i + 1}"
-            cell2.innerHTML = "${document.getElementById("audioPlayer").src}"
-            cell3.innerText = "Audio"
-            localStorage.i += 1;
-		}</script>`
+
 	}
 	if (req.query.videoFile) {
 		sentMessage += `</br> <video id = "videoPlayer" src = ${req.query.videoFile}></video>`;
@@ -29,18 +18,6 @@ app.get('/', (req, res) => {
 		sentMessage += `<button type="button" id = "videoAdd">Add video</button>`;
 		sentMessage += `<script>document.getElementById("videoCancel").addEventListener('click', 
 		() => document.getElementById("videoPlayer").src = "cancel.mp4")</script>`
-		sentMessage += `<script defer>document.getElementById("videoAdd").addEventListener('click', 
-		() => {
-    		let table = document.getElementById("playlist_table");
-    		let row = table.insertRow(localStorage.i);
-    		let cell1 = row.insertCell(0);
-			let cell2 = row.insertCell(1);
-            let cell3 = row.insertCell(2);
-            cell1.innerHTML = "${localStorage.i + 1}"
-            cell2.innerHTML = "${document.getElementById("audioPlayer").src}"
-            cell3.innerText = "Audio"
-            localStorage.i += 1;
-		}</script>`
 	}
 	if (req.query.imgFile) {
 		sentMessage += `<img src=${req.query.imgFile} id = "posterImage">`;
@@ -53,6 +30,34 @@ app.get('/', (req, res) => {
     				  	<th>Type</th>
   					  </tr>
 					</table>`
+
+	if (req.query.audioFile) {
+		sentMessage += `<script defer>document.getElementById("audioAdd").addEventListener('click', 
+		() => {
+    		let table = document.getElementById("playlist_table");
+    		let row = table.insertRow(table.rows.length);
+    		let cell1 = row.insertCell(0);
+			let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            cell1.innerHTML = "${table.rows.length + 1}"
+            cell2.innerHTML = "${document.getElementById("audioPlayer").src}"
+            cell3.innerText = "Audio"
+		}</script>`
+	}
+
+	if (req.query.videoFile) {
+		sentMessage += `<script defer>document.getElementById("videoAdd").addEventListener('click', 
+		() => {
+    		let table = document.getElementById("playlist_table");
+    		let row = table.insertRow(table.rows.length);
+    		let cell1 = row.insertCell(0);
+			let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            cell1.innerHTML = "${table.rows.length + 1}"
+            cell2.innerHTML = "${document.getElementById("audioPlayer").src}"
+            cell3.innerText = "Audio"
+		}</script>`
+	}
 
 
 
